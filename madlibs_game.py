5e291847b799ff1e03b343237dd5f84aa3caa38b
sanyue <92298701+sanyuezoe@users.noreply.github.com>
2022-01-26 23:10:14 +0100

    Add files via upload

---
+++ madlibs_game.py
@@ -0,0 +1,13 @@
+adj = input("Adjective: ")
+a_person = input("person's name: " )
+a_place = input("Place: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+
+madlib = f"This is a {adj} day. {a_person} and me went to a {a_place}. \
+    I saw a cute puppy there then I started to {verb1}, the puppy owner kept {verb2}. \
+        at the end, we {verb3} "
+
+
+print(madlib)
